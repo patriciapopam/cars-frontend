58643913792244faa4353fcce283d646992f2004
patriciapopam <patricia.popamihai@gmail.com>
2023-07-26 21:24:57 +0300

    added footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+  currentYear: number = new Date().getFullYear();
+  appName = "Cars App";
+  
   constructor() { }
 
   ngOnInit(): void {
