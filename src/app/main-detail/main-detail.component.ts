78657f778b7ee5341543d8fcbd96e9b1938bb5e1
patriciapopam <patricia.popamihai@gmail.com>
2023-07-27 21:30:44 +0300

    table highlight and details

--- src/app/main-detail/main-detail.component.ts
+++ src/app/main-detail/main-detail.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+import {animate, state, style, transition, trigger} from '@angular/animations';
 import { HttpExampleService } from '../http-example.service';
 
 @Component({
   selector: 'app-main-detail',
   templateUrl: './main-detail.component.html',
-  styleUrls: ['./main-detail.component.css']
+  styleUrls: ['./main-detail.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class MainDetailComponent implements OnInit {
   data: any[] = [];
-  displayedColumns = ['brand', 'color', 'model', 'category', 'engine', 'year'];
+  displayedColumns = ['brand', 'color', 'model', 'category', 'year', 'country'];
+
+  expandedCar: any = null;
+
+  //[{"id":"111","brand":"Dacia","color":"blue","model":"Duster","category":"hatchback","engine":"petrol","year":"2017","fuelType":"petrol","cylinderCapacity":"100","torque":"100","horsePower":"100","country":"romania","transmission":"100"},
+  //id
+  //brand, model, category, color, year, country
+  //engine, fuelType, cylinderCapacity, torque, horsePower, transmission
 
   constructor(private httpService: HttpExampleService) { }
 
@@ -17,7 +32,6 @@ export class MainDetailComponent implements OnInit {
     this.fetchData();
   }
 
-  
   fetchData() {
     this.httpService.fetchData().subscribe(
       (data) => {
@@ -25,5 +39,4 @@ export class MainDetailComponent implements OnInit {
       }
     );
   }
-
 }
