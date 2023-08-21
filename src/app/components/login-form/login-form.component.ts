64e4b6d78d5b297b2c4a6d87e0e190b7d194a1cb
Alex <aconstantin86@yahoo.com>
2023-08-21 20:58:47 +0300

    handled status codes for login

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,21 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/AuthService/auth-services.service';
+import { SnackbarService } from 'src/app/services/SnackBarService/snack-bar-service.service';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.css']
+  styleUrls: ['./login-form.component.css'],
 })
 export class LoginFormComponent {
-  email : string ="";
-  password : string ="";
+  constructor(
+    private authService: AuthService,
+    private SnackBarService: SnackbarService
+  ) {}
+  email: string = '';
+  password: string = '';
 
-  submit(){
-    console.log("user name is " + this.email)
+  submit() {
+    console.log('user name is ' + this.email);
+    this.authService.login(this.email, this.password).subscribe(
+      (response) => {
+        // Check the status code and handle it accordingly
+        if (response.status === 200) {
+          // Successful login
+          this.SnackBarService.showSuccessSnackbar('Login successful');
+        } else if (response.status === 403) {
+          console.log('Status code 403: Forbidden');
+          this.SnackBarService.showErrorSnackbar(
+            'Username or password incorrect'
+          );
+          // Handle forbidden access
+        }
+      },
+      (error) => {
+        console.log('Login error:', error);
+        this.SnackBarService.showErrorSnackbar(
+          'Login failed, check console output for details'
+        );
+        // Handle error cases
+      }
+    );
     this.clear();
   }
-  
-  clear(){
-    this.email ="";
-    this.password = "";
+
+  clear() {
+    this.email = '';
+    this.password = '';
   }
 }
