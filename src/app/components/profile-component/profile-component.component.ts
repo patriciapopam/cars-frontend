88267f526a270c52d11b2d0f43374abe348cbba4
Alex <aconstantin86@yahoo.com>
2023-08-23 19:22:36 +0300

    the text on the left of the page does not change anymore if only the text inside of the form changes

--- src/app/components/profile-component/profile-component.component.ts
+++ src/app/components/profile-component/profile-component.component.ts
@@ -6,13 +6,23 @@ import { AuthService } from 'src/app/services/AuthService/auth-services.service'
   templateUrl: './profile-component.component.html',
   styleUrls: ['./profile-component.component.css'],
 })
-export class ProfileComponentComponent {
+export class ProfileComponentComponent implements OnInit {
   constructor(public authService: AuthService) {}
-
+  ngOnInit(): void {
+    this.firstNameCopy = this.firstName;
+    this.lastNameCopy = this.lastName;
+  }
+  firstNameCopy: string = '';
+  lastNameCopy: string = '';
+  fieldsDisabled: boolean = true;
   firstName: string = this.authService.user!.firstName;
   lastName: string = this.authService.user!.lastName;
   username: string = this.authService.user!.username;
   email: string = this.authService.user!.email;
+
+  editProfile() {
+    this.fieldsDisabled = false;
+  }
 
   saveChanges() {
     // Implement logic to save changes to a backend or perform desired actions
